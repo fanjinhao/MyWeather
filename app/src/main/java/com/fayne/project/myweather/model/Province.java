33862145a73bfc00daa2d297b95eabb61a79b95f
Finhoo <fjinhao@qq.com>
2017-10-26 20:12:48 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/fayne/project/myweather/model/Province.java
@@ -0,0 +1,36 @@
+package com.fayne.project.myweather.model;
+
+/**
+ * Created by fan on 2017/10/26.
+ */
+
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
---
+++ app/src/main/java/com/fayne/project/myweather/util/HttpUtil.java
@@ -0,0 +1,9 @@
+package com.fayne.project.myweather.util;
+
+/**
+ * Created by fan on 2017/10/26.
+ */
+
+public class HttpUtil {
+
+}
